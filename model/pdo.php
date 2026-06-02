ea332fd2bf70bb29b15bdbba58279ffa7da36ff4
Axel Beaulieu <axel.b.luangkham@gmail.com>
2026-06-02 22:51:53 +0200

    feat: that's what I'm talking about. New database change

--- model/pdo.php
+++ model/pdo.php
@@ -1,9 +1,18 @@
 <?php
+$lines = file(__DIR__ . '/../.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+foreach ($lines as $line) {
+    if (strpos($line, '=') !== false && strpos($line, '#') !== 0) {
+        [$key, $value] = explode('=', $line, 2);
+        $_ENV[trim($key)] = trim($value);
+    }
+}
+
 class CoPDO {
-    private $host = 'localhost';
-    private $db_name = 'ppe';
-    private $username = 'root';
-    private $password = 'FrouFrou';
+    private $host;
+    private $port;
+    private $db_name;
+    private $username;
+    private $password;
     private $con;
 
     public function co_pdo() {
@@ -11,9 +20,9 @@ class CoPDO {
 
         try {
             $this->con = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                $this->username, 
-                $this->password, 
+                "mysql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'], 
+                $_ENV['DB_USER'], 
+                $_ENV['DB_PASSWORD'], 
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
             );
         }
