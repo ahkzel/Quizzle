1f845115f5293c44b89eb72223924f0aac2ce35c
Axel Beaulieu <axel.b.luangkham@gmail.com>
2026-06-07 23:04:12 +0200

    feat: debug

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,12 +1,15 @@
-document.getElementById("theme-filter").addEventListener("change", function() {
-    const theme = this.value.toLowerCase();
-    const rows = document.querySelectorAll(".quizz-table tbody tr");
+const themeFilter = document.getElementById("theme-filter");
+if (themeFilter) {
+    themeFilter.addEventListener("change", function() {
+        const theme = this.value.toLowerCase();
+        const rows = document.querySelectorAll(".quizz-table tbody tr");
 
-    rows.forEach(row => {
-        const rowTheme = row.querySelector("td:nth-child(2)").textContent.toLowerCase();
-        row.style.display = (theme === "" || rowTheme === theme) ? "" : "none";
-    });
-});
+        rows.forEach(row => {
+            const rowTheme = row.querySelector("td:nth-child(2)").textContent.toLowerCase();
+            row.style.display = (theme === "" || rowTheme === theme) ? "" : "none";
+        });
+    })
+}
 
 function processData(responses) {
     const counts = {};
